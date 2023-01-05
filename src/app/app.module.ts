3757e67644cbd07d17e239cd1a23dde43d8bf8c5
progikusok <moyomylo11@gmail.com>
2023-01-05 14:41:12 +0300

    feature: initial: add nrwl moonrepo

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+
+@NgModule({
+  declarations: [AppComponent, MainPageComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
